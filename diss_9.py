21a885b9cd931346fb4baba91d8176a1c82c4aab
ehanafi <ehanafi@umich.edu>
2021-03-29 23:24:45 -0400

    time time complete

--- diss_9.py
+++ diss_9.py
@@ -6,34 +6,61 @@ import unittest
 # Task 1: Get the URL that links to the Pokemon Charmander's webpage.
 # HINT: You will have to add https://pokemondb.net to the URL retrieved using BeautifulSoup
 def getCharmanderLink(soup):
+    
+    # print(soup)
+
+    # Extract info from the page
+    # Get all tags of a certain type from the soup
+    # pokedex = soup.find_all('a', href="/pokedex/national")
+
+    # print(pokedex)
+
     pass
 
 # Task 2: Get the details from the box below "Egg moves". Get all the move names and store
-#         them into a list. The function should return that list of moves.
+# them into a list. The function should return that list of moves.
 def getEggMoves(pokemon):
     url = 'https://pokemondb.net/pokedex/'+pokemon
+    
     #add code here
+    move_names = []
 
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    # Extract info from the page
+    # Get all tags of a certain type from the soup
+    tags = soup.find_all('p', class_="text-small")
+
+    for tag in tags:
+        if "<a href=/pokedex/scizor/egg" in tag:
+            print(tag)
+
+
+# done
 # Task 3: Create a regex expression that will find all the times that have these formats: @2pm @5 pm @10am
 # Return a list of these times without the '@' symbol. E.g. ['2pm', '5 pm', '10am']
+
 def findLetters(sentences):
     # initialize an empty list
-    
+    times = []
 
     # define the regular expression
+    regex = r"@[0-9]{1,2}\s*[amp]+"
     
 
     # loop through each sentence or phrase in sentences
+    for sentence in sentences:
+        sentence = sentence.rstrip()
     
+        # find all the words that match the regular expression in each sentence
+        words = re.findall(regex, sentence)
 
-    # find all the words that match the regular expression in each sentence
-       
+        # loop through the found words and add the words to your empty list
+        for word in words:
+            times.append(word[1:])
 
-    # loop through the found words and add the words to your empty list
-
-
-    #return the list of the last letter of all words that begin or end with a capital letter
-
+    return times
 
 
 def main():
